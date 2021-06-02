1f34d04b5acb601cebb5c8123672dd19e8b85f22
Emma Wu <xinyi.wu@mail.mcgill.ca>
2021-06-02 22:08:48 +0800

    Print received bytes

--- python-socket/np-client.py
+++ python-socket/np-client.py
@@ -2,10 +2,12 @@
 import socket
 from timeit import default_timer as timer
 
-HOST = '127.0.0.1'  # local host
-#HOST = '132.206.51.95' # IP of mimi
+#HOST = '127.0.0.1'  # local host
+HOST = '132.206.51.95' # IP of mimi
 #HOST = '10.0.0.20' # IP of Emma
 PORT = 60000        # The port used by the server
+
+size = 0
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
@@ -13,16 +15,18 @@ print("Connected\n")
 
 #Receive a small packet from the server
 data = s.recv(4096)
-print("Small packet received\n")
+print("Small packet received, in bytes:", len(data))
 
 #Receive formal data from the server
 start = timer()
 
 while True:
     data = s.recv(4096)
+    size += len(data)
     if not data:
         break
-print("All data received\n")
+
+print("All data received, in bytes:", size)
 
 end = timer()
 print("The elapsed time in seconds:", end - start)
