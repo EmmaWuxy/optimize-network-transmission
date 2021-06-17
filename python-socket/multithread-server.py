ed3ee8507615da527bc002cef3c0df818f575288
Emma Wu <xinyi.wu@mail.mcgill.ca>
2021-06-17 19:41:45 +0800

    Modify multithread python program

--- python-socket/multithread-server.py
+++ python-socket/multithread-server.py
@@ -24,21 +24,26 @@ def receive(sock):
     unpickler = pickle.Unpickler(sock.makefile(mode='rb'))
     return unpickler.load()
 
-def threaded(name):
+def threaded(name, p):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(1)
+        s.bind((HOST, p))
+        s.listen(5)
         conn, addr = s.accept()
-        print('Connected by socket no. {}, address {}'.format(name, addr))
+        print('Connected by socket no.{}, address {}'.format(name, addr))
         with conn:
             transmit(conn, data)
-            print("Packet {num} successfully sent the formal data packets")
+            print(f"Packet {name} successfully sent the formal data packets")
             confirm = receive(conn)
             if confirm != test_size:
                 sys.exit('Error: client do not receive all {} bytes data'.format(test_size))
-            
+
+# Create threads
+t1 = threading.Thread(target=threaded, args=(1, PORT+1))
+t2 = threading.Thread(target=threaded, args=(2, PORT+2))
+t3 = threading.Thread(target=threaded, args=(3, PORT+3))     
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
     s.bind((HOST, PORT)) #binds it to a specific ip and port so that it can listen to incoming requests on that ip and port
     s.listen(1) #puts the server into listen mode, allows the server to listen to incoming connections. Passing an empty string means that the server can listen to incoming connections from other computers as well.
     conn, addr = s.accept() #block and wait for an incoming connection
@@ -48,25 +53,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         transmit(conn, data_pre)
         print("Successfully sent the small packet")
         #Receive the ack from the client of receiving 1000 characters
-        confirm = receive(conn)
-        if confirm != 1000:
-            sys.exit("Error: client do not receive all 1000 bytes data")
+        #confirm = receive(conn)
+        #if confirm != 1000:
+        #    sys.exit("Error: client do not receive all 1000 bytes data")
 
         # Formal test
-        # Create threads
-            t1 = threading.Thread(target=threaded, args=(0,))
-            t2 = threading.Thread(target=threaded, args=(1,))
-            t3 = threading.Thread(target=threaded, args=(2,))
-
         #Start threads
-            t1.start()
-            t2.start()
-            t3.start()
+        t1.start()
+        t2.start()
+        t3.start()
 
         # Wait until all threads finish their jobs
-            t1.join()
-            t2.join()
-            t3.join()
+        t1.join()
+        t2.join()
+        t3.join()
 
         # Send back a request for closing
         transmit(conn, 0)
